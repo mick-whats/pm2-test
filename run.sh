33c7956c1cb2c00c0f59d8dbb2d6fe35b65bd2cb
whats-mick <whats_2013@icloud.com>
2015-03-25 15:31:02 +0900

    deploy test

---
+++ run.sh
@@ -0,0 +1,1 @@
+~/.nodebrew/node/v0.12.0/bin/node test.js
--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 var http = require('http');
 
-PORT_NUMBER = process.env.port || 9876
+PORT_NUMBER = process.env.PORT || 9876
 http.createServer(function(request, response) {
   response.writeHead(200, {
     'Content-Type': 'text/plain'
   });
-  response.end('Hello World\nnode version:' + process.version + '\npid: ' + process.pid););
+  response.end('Hello World\nnode version:' + process.version + '\npid: ' + process.pid);
 }).listen(PORT_NUMBER);
 
 console.log('Server running at http://127.0.0.1:' + PORT_NUMBER);
